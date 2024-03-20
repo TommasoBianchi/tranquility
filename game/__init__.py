Fix computation of cards to discard when the start is played

game/__init__.py
@@ -29,8 +29,9 @@ def run_game(seed: int, config: GameConfig, player_types):
     # Init board
     board = Board(size=config.board_size, n_cards=config.n_cards)
 
-    start_discards = [int(np.ceil(config.start_discard_size // config.n_players))] * (config.n_players - 1)
-    start_discards.append(config.start_discard_size - sum(start_discards))
+    start_discards = [config.start_discard_size // config.n_players] * config.n_players
+    for i in range(config.start_discard_size - sum(start_discards)):
+        start_discards[i] += 1
 
     start, start_player_id = False, -1
     win, lose = False, False

game/config.py
@@ -9,11 +9,12 @@ class GameConfig:
     n_cards: int = 80
     n_finish: int = 5
     start_discard_size: int = 2
-    pass_discard_size: int = 2
+    pass_discard_size: int = 8
     
     def __post_init__(self):
         assert self.n_players > 0
         assert self.board_size > 0 and self.board_size < self.n_cards
         assert self.n_finish > 0
         assert self.n_cards >= min(1, np.ceil(self.n_players * self.hand_sizes) - self.n_finish)
-        assert self.hand_sizes >= max([self.pass_discard_size, self.start_discard_size, np.ceil(self.start_discard_size / self.n_players)])
+        assert self.hand_sizes >= max([self.pass_discard_size, np.ceil(self.start_discard_size / self.n_players)])
+        assert self.start_discard_size < self.hand_sizes * self.n_players