59746d8c56489f826f1181ff5c746b69d57e4b55
Tommaso Bianchi <tommaso.bianchi@gmail.com>
2024-03-27 21:43:24 +0100

    Fix logic for dealing decks to players and add some extra assertions to monitor it

--- game/game_setup.py
+++ game/game_setup.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def setup_game(n_players, n_cards=80, n_finish=5, hand_sizes=5, seed=17):
     # define cards (except starts)
-    cards = list(np.hstack((np.repeat(n_cards+1,n_finish), np.arange(1, n_cards))))
+    cards = list(np.hstack((np.repeat(n_cards+1,n_finish), np.arange(1, n_cards+1))))
     deck_len = int(np.ceil((len(cards)+1)//n_players))
     # shuffle cards
     np.random.seed(seed)
@@ -11,6 +11,8 @@ def setup_game(n_players, n_cards=80, n_finish=5, hand_sizes=5, seed=17):
     decks = [cards[i*deck_len:(i+1)*deck_len] for i in range(n_players-1)]
     decks.append(cards[(n_players-1)*deck_len:])
     assert len(decks)==n_players
+    assert len(set(np.concatenate(decks)))==n_cards+1, f"Cards are missing from decks dealt to players: {set(cards) - set(np.concatenate(decks))}"
+    assert sum([len(deck) for deck in decks])==n_cards+n_finish, f"{n_cards - sum([len(deck) for deck in decks])} cards are missing from decks"
     # generate starting hands
     hands = [[deck.pop() for _ in range(hand_sizes)] for deck in decks]
     # add starts to decks
