a2a5305a0f090f361c84886554b9347d53858281
Tommaso Bianchi <tommaso.bianchi@gmail.com>
2024-03-30 19:56:25 +0100

    Add precomputation of empty board positions in Board to optimize

--- game/board.py
+++ game/board.py
@@ -16,6 +16,7 @@ class Board:
         self._board_sha = None
         self.min_board, self.max_board = np.repeat(np.nan, size), np.repeat(np.nan, size)
         self.start, self.finish = False, False
+        self.__update_board_internals()
 
     def __board_changed(self):
         current_board_sha = sha512(np.ascontiguousarray(self.board)).hexdigest()
@@ -24,10 +25,11 @@ class Board:
             return True
         return False
 
-    def __update_minmax_board(self):
+    def __update_board_internals(self):
         if self.__board_changed():
             self.max_board = np.nan_to_num(backward_fill_na(self.board), nan=self.n_cards + 1)
             self.min_board = np.nan_to_num(forward_fill_na(self.board), nan=0)
+            self._empty_board_positions = np.isnan(self.board)
 
     def check_if_position_legal(self, new_card, position, hand_size):
         if position < 0 or position >= self.size:
@@ -37,16 +39,16 @@ class Board:
         elif new_card == 0:
             return not self.start
         elif new_card == self.n_cards+1:
-            return (not self.finish) and (sum(np.isnan(self.board))==0)
+            return (not self.finish) and (self._empty_board_positions.sum() == 0)
         elif self.get_action_cost(new_card, position) <= hand_size:
-            self.__update_minmax_board()
+            self.__update_board_internals()
             return ((self.min_board <= new_card) * (self.max_board >=new_card))[position]
         else:
             return False
     
     def get_action_cost(self, new_card, position):
-        left_nan = position == 0 or np.isnan(self.board[position - 1])
-        right_nan = position == self.size - 1 or np.isnan(self.board[position + 1])
+        left_nan = position == 0 or self._empty_board_positions[position - 1]
+        right_nan = position == self.size - 1 or self._empty_board_positions[position + 1]
 
         if left_nan and right_nan:
             return 0
@@ -58,19 +60,19 @@ class Board:
             return int(min([new_card - self.board[position - 1], self.board[position + 1] - new_card]))
     
     def check_completion(self):
-        return bool(self.start*self.finish*(sum(np.isnan(self.board))==0))
+        return bool(self.start * self.finish * (self._empty_board_positions.sum() == 0))
     
     def receive_card(self, new_card, position, hand_size):
         if new_card == 0:
             assert not self.start
             self.start = True
         elif new_card == self.n_cards+1:
-            assert (not self.finish)*(sum(np.isnan(self.board))==0)
+            assert (not self.finish) * (self._empty_board_positions.sum() == 0)
             self.finish = True
         else:
             assert self.check_if_position_legal(new_card, position, hand_size)
             self.board[position] = new_card
-            self.__update_minmax_board()
+            self.__update_board_internals()
         return self.board
     
     
