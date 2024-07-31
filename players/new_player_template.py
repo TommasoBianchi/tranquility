a463a963c4cd2eaf97cd6bf0b426737fd45e67e7
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-07-31 09:35:36 +0000

    Run Ruff format on all python files

--- players/new_player_template.py
+++ players/new_player_template.py
@@ -1,12 +1,13 @@
 import numpy as np
 from game.player import Player
 
+
 class HeuristicPlayer(Player):
     def __init__(self, id, n_cards, n_players, pass_discard_size, deck, initial_hand):
         super().__init__(id, n_cards, n_players, pass_discard_size, deck, initial_hand)
 
     def decide_discards_start(self, n_to_discard):
         pass
-    
+
     def decide_action(self):
         pass
